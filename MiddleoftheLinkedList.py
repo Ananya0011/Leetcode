6f38695fa5ab2cf090730e307a9b67c24bd2174b
Ananya0011 <ananyaop988@gmail.com>
2026-06-27 01:39:26 +0530

    Implement function to find middle of linked list

---
+++ MiddleoftheLinkedList.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        d=head
+        tail=head
+        while tail and tail.next:
+            d=d.next
+            tail=tail.next.next
+        return d
+        
